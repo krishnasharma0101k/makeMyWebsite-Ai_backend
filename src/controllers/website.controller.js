a8f590b945bea1930d54745dbc65ece637576083
krishna Sharma <krishnasharma02309@gmail.com>
2026-07-19 01:48:10 +0530

    website deploy

--- src/controllers/website.controller.js
+++ src/controllers/website.controller.js
@@ -262,6 +262,11 @@ export const change = async (req, res) => {
             _id: req.params.id,
             user: req.user._id
         })
+
+        if (!website) {
+            return res.status(400).json({message: "website not found"})
+        }
+
         const user = await User.findById(req.user._id);
 
         if(!user) {
@@ -350,3 +355,37 @@ export const getAllWebsites = async (req, res) => {
         });
     }
 }
+
+export const deploy = async (req,res) => {
+    try {
+        const website = await Website.findOne({
+            _id: req.params.id,
+            user: req.user._id
+        })
+
+        if (!website) {
+            return res.status(400).json({message: "website not found"})
+        }
+
+        if (!website.slug) {
+            website.slug = website.title
+            .toLowerCase()
+            .replace(/[^a-z0-9]/g,"")
+            .slice(0,60) +
+             website._id
+             .toString().slice(-5)
+        }
+
+
+        website.deploy = true
+        website.deployUrl = `${process.env.FRONTEND_URL}/site/${website.slug}`
+        await website.save()
+
+        return res.status(200).json({url: website.deployUrl})
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `deploy website error: ${error}`
+        });
+    }
+} 
--- src/routes/website.route.js
+++ src/routes/website.route.js
@@ -2,7 +2,8 @@ import express from "express"
 
 import isauth from "../middlewares/auth.middleware.js"
 import { generateWebsite, getWebsiteById, change,
-    getAllWebsites
+    getAllWebsites,
+    deploy
  } from "../controllers/website.controller.js"
 
 const websiteRouter = express.Router()
@@ -11,5 +12,6 @@ websiteRouter.post("/generate", isauth, generateWebsite)
 websiteRouter.post("/update/:id", isauth, change)
 websiteRouter.get("/get-by-id/:id", isauth, getWebsiteById)
 websiteRouter.get("/get-all", isauth, getAllWebsites)
+websiteRouter.get("/deploy", isauth, deploy)
 
 export default websiteRouter
